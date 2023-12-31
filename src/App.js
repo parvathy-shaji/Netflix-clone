8fb3777e913705c0daa885b7da8073c8a0b96d3f
parvathy-shaji <parvathyshaji000@gmail.com>
2024-01-01 01:39:40 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import Row from './components/Row';
+import requests from './requests';
+import Banner from './components/Banner';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div>
+
+      <Nav/>
+
+      <Banner fetchUrl={requests.fetchNetflixOriginals}/>
+
+      <Row isPresent={true} title="NetflixOriginals" fetchUrl={requests.fetchNetflixOriginals}/>
+
+      <Row title="Trending" fetchUrl={requests.fetchTrending}/>
+
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+
+    </div>
+  );
+}
+
+export default App;
